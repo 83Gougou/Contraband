4d72ae0f07a405816efe8da350b70d8566910281
83Gougou <theronbil83@gmail.com>
2026-06-25 19:05:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// servir les fichiers statiques (ici juste index.html)
+// Sert les fichiers statiques (index.html)
 app.use(express.static(path.join(__dirname)));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`Serveur Contraband lancé sur le port ${PORT}`);
+  console.log(`Serveur lancé sur le port ${PORT}`);
 });
