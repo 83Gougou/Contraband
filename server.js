bac52aee2b5aebe2c722d049e32c2cbd7204fd70
83Gougou <theronbil83@gmail.com>
2026-06-25 19:24:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Sert tous les fichiers du dossier courant (index.html, images, css, js…)
 app.use(express.static(path.join(__dirname)));
 
 app.get("/", (req, res) => {
